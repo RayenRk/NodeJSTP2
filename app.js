dae89b4feb3982215beaecfcbe54ee2c408572d5
RayenRk <rakkadrayen@gmail.com>
2024-02-26 14:45:53 +0100

    🥈 new branch for mongoose

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-const express = require('express');
-const voitures = require('./routes/voitures');
+const express = require("express");
+const voitures = require("./routes/voitures");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/voitures', voitures);
+app.use("/voitures", voitures);
 
-app.listen(4000, () => {
-  console.log('Server started on http://localhost:4000');
+// app.listen(4000, () => {
+//   console.log('Server started on http://localhost:4000');
+// });
+
+mongoose.connect(MONGOBD_URI).then(() => {
+  console.log("connected to database");
+  app.listen(PORT, () => {
+    console.log("Server started on http://localhost:${PORT}");
+  });
 });
